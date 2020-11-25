9ac2b8b124b89f9f56e9c29c9e0d547361ab5e61
naveen <naveendavisv@gmail.com>
2020-11-25 11:10:10 -0800

    combinations qus

---
+++ chapter1/combinations.py
@@ -0,0 +1,7 @@
+# itertools.combinations(iterable, r)
+# Return r length subsequences of elements from the input iterable.
+# Combinations are emitted in lexicographic sort order. So, if the input iterable is sorted, the combination tuples will be produced in sorted order.
+
+#Example:
+#  combinations('ABCD',2)   --> AB,AC,AD,BC,BD,CD
+#  combinations(range(4),3) --> 012,013,023,123
---
+++ chapter1/subsetsum.py
@@ -0,0 +1,9 @@
+#********subset sum problem************
+
+#Let A be an array or set which contains n non negative integers
+#Find a subset X of set A so that the sum of all elements of X = w
+#
+#Example  A = { 1,2,5,9,4}
+#sum w = 18 
+
+
