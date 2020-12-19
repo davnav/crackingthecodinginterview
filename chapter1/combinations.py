721fd7a0c6753c6fc0ed06cbf37883ccd528c82f
naveen <naveendavisv@gmail.com>
2020-12-18 23:31:45 -0800

    combination recursion solution

--- chapter1/combinations.py
+++ chapter1/combinations.py
@@ -5,3 +5,41 @@
 #Example:
 #  combinations('ABCD',2)   --> AB,AC,AD,BC,BD,CD
 #  combinations(range(4),3) --> 012,013,023,123
+
+
+#########Solutions 1 #######################
+#This is for the below type of example where a list of 'n' numbers passed to find
+#all combinations out of it
+
+#  combinations([1,2,3],0,results,[])
+
+import copy
+
+def combinations(li,i,results,path1):
+    
+
+    
+    if i == len(li):
+        results.append(path1)
+        return
+
+    pathCurrent=copy.copy(path1)
+    pathCurrent.append(li[i])
+    print(pathCurrent)
+    
+    #find all combinations that  exclude the current item
+
+    combinations(li,i+1,results,path1)
+    
+    #find all combination that include the current item
+    combinations(li,i+1,results,pathCurrent)
+
+
+li = [1,2,3]
+i=0
+path=[]
+results=[[]]
+
+combinations(li,i,results,path)
+
+print(results)
