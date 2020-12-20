995802fa627367f6b0b5640dd8cb16badedecc0c
naveen <naveendavisv@gmail.com>
2020-12-19 16:03:01 -0800

    sudoku second commit modifications

--- chapter1/sudoku.py
+++ chapter1/sudoku.py
@@ -1,6 +1,6 @@
 #attempt to solve sudoku in python
 
-#it would be example for recursion and backtracking
+#it would be an example for recursion and backtracking
 #example sudoku board below
 board = [[0,0,0,2,6,0,7,0,1],
          [6,8,0,0,7,0,0,9,0],
@@ -11,3 +11,40 @@ board = [[0,0,0,2,6,0,7,0,1],
          [0,0,9,3,0,0,0,7,4],
          [0,4,0,0,5,0,0,3,6],
          [7,0,3,0,1,8,0,0,0]]
+
+def solve(col,row,board):
+    if col >=9:
+       # results.append(board[col][row])
+        row = row+1
+        return 
+   
+    if row >=9:
+        return board
+
+    for  value in range(1,10):
+       
+       if board[col][row] == 0:
+            board[col][row] = value
+            if IsvalidValue(col,row,board):
+                solve(col+1,row,board)
+   
+    board[col][row] = 0
+
+   
+def IsvalidValue(col,row,board):
+  
+    #sudoku rule 1:
+    #search for 3*3 area if find that value, it's invalid selection
+    #eg: if col =2,row =2 
+    #        0,1,2 are col values to search
+    #        0,1,2 are row values to search
+    #eg: if col = 5, row =7
+    #       3*2 = 6 , 5 is below 6 , col area search should be 5,4,3
+    #       3*2 = 6 , row is 7 , greater than 6, so the area for row should be 3*3=9 >(less) 8,7,6 
+    #col-3
+    area =[[0,1,2],[3,4,5],[6,7,8]]
+    
+    if board[col][row] != 0:
+          
+            raise NotImplementedError
+    
